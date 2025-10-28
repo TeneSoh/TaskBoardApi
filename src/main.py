f9779cb62f22c3e4343a9a5ad6158c3182b6b32a
= <tenedouglas237@gmail.com>
2025-10-28 16:12:42 +0100

    adding profile image and test added profile image

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import src.routes.auth.auth as auths
 import src.routes.comments.comments as comments
 import src.routes.projects.projects as projects
 import src.routes.tasks.task as tasks
+import src.routes.users.users as users
 from src.services.services import *
 
 app = FastAPI()
@@ -13,3 +14,4 @@ app.include_router(auths.route)
 app.include_router(projects.route)
 app.include_router(tasks.route)
 app.include_router(comments.route)
+app.include_router(users.route)
